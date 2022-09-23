5498934258c750e61c62a97e07962a0560502b8f
shreyaa12 <shreyagokani31@gmail.com>
2022-09-23 12:33:57 -0600

    routingModule

--- angularProject/src/app/heroes/heroes.component.ts
+++ angularProject/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+// import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,17 +9,26 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  selectedHero?: Hero;
-
-  constructor() { }
+  heroes : Hero[] = [];
+  // selectedHero?: Hero;
+  // , private messageService : MessageService
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
-  selectHero(hero:Hero) :void {
-    console.log(hero)
-    this.selectedHero = hero;
+    // selectHero(hero:Hero) :void {
+    //   console.log(hero)
+    //   this.selectedHero = hero;
+    //   this.messageService.add(`HeroesComponent : Selected Hero id:${hero.id}`);
+    // }
+  getHeroes(): void {
+    console.log("inside get heroes");
+    // this.heroes = this.heroService.getHeroes();        //when local data and no observable
+
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
