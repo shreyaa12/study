6a0da8369c99095856b50a7f728ffcd22644a3d2
shreyaa12 <shreyagokani31@gmail.com>
2022-09-27 14:15:06 -0600

    new Project crud operations

---
+++ CrudForm/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    ListEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
